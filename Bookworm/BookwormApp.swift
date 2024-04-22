5b58f3bd807c83fab80d68bbe10ef28366a9e402
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-04-22 22:17:31 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Carlos Román Alcaide on 22/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
