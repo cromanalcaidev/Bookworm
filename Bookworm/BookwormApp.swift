ffe520b9207004a139ef644f75d9e415fcab3b43
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-04-23 19:13:54 +0200

    Day 54 - Proj. 11, pt. 2

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -5,6 +5,7 @@
 //  Created by Carlos Román Alcaide on 22/4/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Book.self)
     }
 }
--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -5,17 +5,31 @@
 //  Created by Carlos Román Alcaide on 22/4/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add book", systemImage: "plus") {
+                            showingAddScreen.toggle()
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
         }
-        .padding()
     }
 }
 
